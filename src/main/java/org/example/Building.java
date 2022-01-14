501d05bd3cc4fa7951a28d2c8def3521c9062560
SimonSixt <sixtsim@gmail.com>
2022-01-14 12:38:01 +0100

    itShouldCreateResident()

--- src/main/java/org/example/Building.java
+++ src/main/java/org/example/Building.java
@@ -2,23 +2,19 @@ package org.example;
 
 public class Building {
     private Integer mId;
-    private String mInitialResident;
     private String[] mInitialResidents;
 
-    public Building(Integer i, String initialResident){
-
+    public Building(Integer id, String initialResident){
+        String[] s = {initialResident};
+        mInitialResidents = s;
     }
 
     public Building(Integer id, String[] initialResidents){
 
     }
 
-    public String getInitialResident(){
-        return "";
-    }
-
     public String[] getInitialResidents(){
-        return null;
+        return mInitialResidents;
     }
 
     public void addResident(String name){
--- src/test/java/org/example/BuildingTest.java
+++ src/test/java/org/example/BuildingTest.java
@@ -1,4 +1,15 @@
+package org.example;
+
 import junit.framework.TestCase;
-public class BuildingTest extends TestCase {
-  
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BuildingTest {
+
+    @Test
+    public void itShouldCreateResident() {
+        Building building = new Building(0, "name");
+
+        Assert.assertNotNull(building.getInitialResidents());
+    }
 }
