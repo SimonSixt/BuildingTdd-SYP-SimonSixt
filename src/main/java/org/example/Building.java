2db8108473f663151f09340fe6e736a77ae81cba
SimonSixt <sixtsim@gmail.com>
2022-01-14 12:12:04 +0100

    Skeleton Implementation

---
+++ src/main/java/org/example/Building.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Building {
+    private Integer mId;
+    private String mInitialResident;
+    private String[] mInitialResidents;
+
+    public Building(Integer i, String initialResident){
+
+    }
+
+    public Building(Integer id, String[] initialResidents){
+
+    }
+
+    public String getInitialResident(){
+        return "";
+    }
+
+    public String[] getInitialResidents(){
+        return null;
+    }
+
+    public void addResident(String name){
+
+    }
+
+    public void removeResident(String name){
+
+    }
+}
